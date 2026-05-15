97e4255f5284e2a236b0d7e8ec71fcd906860c4f
Diana <dgijrad2710@g.educaand.es>
2026-05-15 18:40:36 +0200

    chore: update API endpoint and configure EBS storage class with deployment documentation

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://a6ca02cd3630044809d900c2bf61c715-138716680.us-east-1.elb.amazonaws.com';
+export const BASE_URL = 'https://ab5ccc212e71a41228d9baabbecb9902-1025918542.us-east-1.elb.amazonaws.com';
 export const API_BASE_URL = `${BASE_URL}/api`;
