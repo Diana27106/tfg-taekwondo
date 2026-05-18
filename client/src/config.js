e1333123e75044df2be273e90ac06c6021dc42cd
Diana <dgijrad2710@g.educaand.es>
2026-05-18 15:47:46 +0200

    config actualizado

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = window.location.origin;
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = window.location.origin;
+//export const BASE_URL = 'http://localhost:8000';
 export const API_BASE_URL = `${BASE_URL}/api`;
